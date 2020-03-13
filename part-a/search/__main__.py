e23dbd63aab2f0e69d6bdfdbf22e9262e87276c4
Alex Westcott <alex.w.westcott@gmail.com>
2020-03-13 17:50:05 +1100

    begin part a implementation

--- part-a/search/__main__.py
+++ part-a/search/__main__.py
@@ -1,15 +1,29 @@
 import sys
 import json
 
-from search.util import print_move, print_boom, print_board
+from util import print_move, print_boom, print_board
+from game import Board, ExpendibotsProblem
+from searching import iterative_deepening_search
 
+GOAL_BOARD = [[0 for _ in range(8)] for _ in range(8)]
 
 def main():
+    '''
     with open(sys.argv[1]) as file:
         data = json.load(file)
+    ''' 
 
-    # TODO: find and print winning action sequence
+    data = {"white": [[1,0,0]], "black": [[1,0,1]]}
 
+
+    board_class = Board(data)
+
+    board = board_class.board
+
+    print(board_class)
+
+    problem = ExpendibotsProblem(board, GOAL_BOARD)
+    print(iterative_deepening_search(problem))
 
 
 if __name__ == '__main__':
---
+++ part-a/search/game.py
@@ -0,0 +1,164 @@
+from searching import Problem
+
+BOOM = 0
+MOVE = 1
+BLACK = 0
+WHITE = 1
+
+class Board:
+    ''' An Expendibots board '''
+    
+    def __init__(self, stacks=None):
+        self.board = [[0 for _ in range(8)] for _ in range(8)]
+        
+        self.n_white = 0
+        self.n_black = 0
+
+        if stacks is not None:
+            self.place_stacks(stacks)        
+    
+    def place_stacks(self, stacks):
+        ''' 
+        Initialises board with stacks from dictionary "stacks" 
+        White stacks are positive numbers
+        Black stacks are negative numbers
+        '''
+        white_stacks = stacks['white']
+        black_stacks = stacks['black']
+
+        for stack in white_stacks:
+            n,x,y = stack
+            self.board[x][y] = n
+            self.n_white += 1
+            
+        for stack in black_stacks:
+            n,x,y = stack
+            self.board[x][y] = -n
+            self.n_black += 1
+
+    def get_all_white_actions(self, board):
+        ''' Returns a list of white's possible actions '''
+        actions = []
+        for x in range(8):
+            for y in range(8):
+                if board[x][y] > 0:
+                    actions += self.get_actions(board, x, y)
+        return actions
+
+    def get_actions(self, board, x, y):
+        ''' Gets the actions of the stack at (x, y) '''
+        # all pieces can boom at their position
+        colour = BLACK if board[x][y] < 0 else WHITE
+        actions = [[BOOM, (x, y)]]
+        # Moves
+        height = abs(board[x][y])
+
+        # all possible move positions on the board, including moves on to oposing pieces
+        all_coords = []
+        for dx in range(1, height+1):
+            if 0 <= x+dx < 8:
+                all_coords.append((x+dx, y))
+            if 0 <= x-dx < 8:
+                all_coords.append((x-dx, y))
+
+        for dy in range(1, height+1):
+            if 0 <= y+dy < 8:
+                all_coords.append((x, y+dy))
+            if 0 <= y-dy < 8:
+                all_coords.append((x, y-dy))
+
+        # make sure we dont move onto an opposing colour
+        for destination in all_coords:
+            x1,y1 = destination
+            if board[x1][y1] == 0:
+                continue
+            dest_colour = BLACK if board[x1][y1] < 0 else WHITE
+                
+            if colour != dest_colour:
+                all_coords.remove(destination)
+
+        # append all the moves with all possible number of pieces moved
+        for n in range(1, height+1):
+            for x_dest, y_dest in all_coords:
+                actions.append([MOVE, (n, x, y, x_dest, y_dest)])
+
+        return actions   
+    
+
+    
+    def get_explode_positions(self):
+        ''' Returns a list of coordinates for the AI to explode at '''
+        # only_black_board is the board with only black pieces
+        only_black_board = [[(lambda stack: stack if stack <= 0 else 0)(stack) for stack in row] for row in self.board]
+        # Unexploded is a list of black pieces that are not yet in range by current planned explosions
+        unexploded = [(x, y) for x in range(8) for y in range(8) if only_black_board[x][y] < 0]
+        # Planned is a list of coordinates where we plan to explode
+        planned = []
+
+        while unexploded:
+            biggest = 0
+            # Find coordinates that maximise explosions
+            for x in range(8):
+                for y in range(8):
+                    if board[x][y] == 0:
+                        tmp = [row[:] for row in only_black_board]
+                        tmp, n_explosions = explode(tmp, x, y)
+
+
+    def explode(self, board, x, y, n_explosions=0):
+        ''' Returns board once explosion has occurred at coordinates '''
+        
+        # radius is a list of all board positions to blow up
+        radius = [(x_, y_) for x_ in range(x-1, x+2) for y_ in range(y-1, y+2) if 0 <= x_ < 8 and 0 <= y_ < 8]
+        
+        # Try each position
+        for x,y in radius:
+            # If there's a piece there
+            if board[x][y]:
+                board[x][y] = 0
+                n_explosions += 1
+                board, n_explosions = self.explode(board, x, y, n_explosions)
+            
+        return board, n_explosions 
+            
+
+    def move(self, board, n, x1, y1, x2, y2):
+        '''
+        Moves a specified number of pieces of a stack to a specified spot
+        Assumes move is valid
+        '''
+        
+        if board[x1][y1] > 0:
+            board[x1][y1] -= n
+            board[x2][y2] += n
+        # black stacks have negative values
+        elif board[x1][y1] < 0:
+            board[x1][y1] += n
+            board[x2][y2] -= n
+
+        return board
+
+    def __str__(self):
+        return '\n'.join([''.join([str(space).rjust(3) for space in row]) for row in self.board])
+
+class ExpendibotsProblem(Problem):
+    def actions(self, state):
+        return Board().get_all_white_actions(state)
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        
+        action_type = action[0]
+        if action_type == MOVE:
+            n, x1, y1, x2, y2 = action[1]
+            return Board().move(state, n, x1, y1, x2, y2)
+        elif action_type == BOOM:
+            x, y = action[1]
+            board, n_explosions = Board().explode(state, x, y)
+            return board
+
+
+
+
