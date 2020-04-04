088ee89450a9324169ff5e4b625e3f4bd3e7d7aa
Alex Westcott <alex.w.westcott@gmail.com>
2020-04-04 14:45:00 +1100

    random actions aima player

--- part-b/2020-part-B-skeleton/aima/player.py
+++ part-b/2020-part-B-skeleton/aima/player.py
@@ -1,48 +1,121 @@
+SELF = 1
+OTHER = -1
+BOOM = 0
+MOVE = 1
+
 
 class ExamplePlayer:
-    def __init__(self, colour):
-        """
-        This method is called once at the beginning of the game to initialise
-        your player. You should use this opportunity to set up your own internal
-        representation of the game state, and any other information about the 
-        game state you would like to maintain for the duration of the game.
 
-        The parameter colour will be a string representing the player your 
-        program will play as (White or Black). The value will be one of the 
-        strings "white" or "black" correspondingly.
-        """
-        # TODO: Set up state representation.
+    def __init__(self, colour):
+        self.colour = colour
+        self.board = [
+            [1, 1, 0, 0, 0, 0, -1, -1],
+            [1, 1, 0, 0, 0, 0, -1, -1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, -1, -1],
+            [1, 1, 0, 0, 0, 0, -1, -1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, -1, -1],
+            [1, 1, 0, 0, 0, 0, -1, -1]
+        ]
 
 
     def action(self):
-        """
-        This method is called at the beginning of each of your turns to request 
-        a choice of action from your program.
-
-        Based on the current state of the game, your player should select and 
-        return an allowed action to play on this turn. The action must be
-        represented based on the spec's instructions for representing actions.
-        """
-        # TODO: Decide what action to take, and return it
-        return ("BOOM", (0, 0))
-
+        possible_actions = self.get_all_own_actions(self.board)
+        from random import choice
+        return self.format_action(choice(possible_actions))
 
     def update(self, colour, action):
-        """
-        This method is called at the end of every turn (including your player’s 
-        turns) to inform your player about the most recent action. You should 
-        use this opportunity to maintain your internal representation of the 
-        game state and any other information about the game you are storing.
+        if action[0] == 'MOVE':
+            self.move(self.board, action)
+        else:
+            x0, y0 = action[1]
+            self.explode_recursive(self.board, x0, y0)
 
-        The parameter colour will be a string representing the player whose turn
-        it is (White or Black). The value will be one of the strings "white" or
-        "black" correspondingly.
+        # invert the sign of the pieces so that positive has the next move
+        self.board = [[-n for n in row] for row in self.board]
 
-        The parameter action is a representation of the most recent action
-        conforming to the spec's instructions for representing actions.
 
-        You may assume that action will always correspond to an allowed action 
-        for the player colour (your method does not need to validate the action
-        against the game rules).
-        """
-        # TODO: Update state representation in response to action.
+    def get_all_own_actions(self, board):
+        ''' Returns a list of positive players's possible actions '''
+        actions = []
+        for x in range(8):
+            for y in range(8):
+                if board[x][y] > 0:
+                    actions += self.get_stack_actions(board, x, y)
+        return actions
+
+    def get_stack_actions(self, board, x, y):
+        ''' Gets the actions of the stack at (x, y) '''
+        # all pieces can boom at their position
+        colour = OTHER if board[x][y] < 0 else SELF
+        actions = [[BOOM, (x, y)]]
+        # Moves
+        height = abs(board[x][y])
+
+        # all possible move positions on the board, including moves on to oposing pieces
+        all_coords = []
+        for dx in range(1, height+1):
+            if 0 <= x+dx < 8:
+                all_coords.append((x+dx, y))
+            if 0 <= x-dx < 8:
+                all_coords.append((x-dx, y))
+
+        for dy in range(1, height+1):
+            if 0 <= y+dy < 8:
+                all_coords.append((x, y+dy))
+            if 0 <= y-dy < 8:
+                all_coords.append((x, y-dy))
+
+        # make sure we dont move onto an opposing colour
+        all_coords = [(x1, y1) for x1, y1 in all_coords if (board[x1][y1] == 0) or (board[x1][y1]*colour > 0)]
+
+        # append all the moves with all possible number of pieces moved
+        for n in range(1, height+1):
+            for x_dest, y_dest in all_coords:
+                actions.append([MOVE, (n, x, y, x_dest, y_dest)])
+
+        return actions
+
+    def explode(self, board, x, y):
+        ''' Returns board once explosion has occurred at coordinates 
+        This calls a recursive function once a copy of the board has been made such that the original board is not modified '''
+        return self.explode_recursive([row[:] for row in board], x, y, n_explosions=0)
+    
+    def explode_recursive(self, board, x, y, n_explosions=0):
+        ''' Returns board once explosion has occurred at coordinates '''
+        # radius is a list of all board positions to blow up
+        radius = [(x_, y_) for x_ in range(x-1, x+2) for y_ in range(y-1, y+2) if 0 <= x_ < 8 and 0 <= y_ < 8]
+        
+        # Try each position
+        for x,y in radius:
+            # If there's a piece there
+            if board[x][y]:
+                n_explosions += abs(board[x][y])
+                board[x][y] = 0
+                board, n_explosions = self.explode_recursive(board, x, y, n_explosions)
+            
+        return board, n_explosions 
+
+    def move(self, board, action):
+        '''
+        Move n pieces of a stack from their origin to a destination
+        on a board, assuming valid input. Board is mutated in place
+        '''
+        move, n, orig, dest = action
+        x0, y0 = orig
+        x1, y1 = dest
+        board[x0][y0] -= n
+        board[x1][y1] += n
+
+    def format_action(self, action):
+        if action[0] == BOOM:
+            x,y = action[1]
+            statement = ('BOOM', (x,y))
+        else:
+            n, x, y, x_dest, y_dest = action[1]
+            statement = ('MOVE', n, (x,y), (x_dest, y_dest))
+        return statement
+
+
+
