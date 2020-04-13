5d9630119ab1f41b197c013ad9e1c7449838d4b1
Alex Westcott <alex.w.westcott@gmail.com>
2020-04-13 17:32:34 +1000

    tdleaf, graphs, features
    Co-authored-by: Cameron Chandler <camchandler1@gmail.com>

--- part-b/2020-part-B-skeleton/voltage/treestrap.py
+++ part-b/2020-part-B-skeleton/voltage/treestrap.py
@@ -3,211 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import defaultdict as dd
+from features import Φ, ALL_STACKS, RINGS
 
 INF = 99.0
 TRAIN_DEPTH = 2
-ALL_STACKS = 0
-
-R3 = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), 
-      (7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7),
-      (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0),
-      (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7)]
-      
-R2 = [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
-      (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6),
-      (2, 1), (3, 1), (4, 1), (5, 1), 
-      (2, 6), (3, 6), (4, 6), (5, 6)]   
-
-R1 = [(2, 2), (2, 3), (2, 4), (2, 5),
-      (5, 2), (5, 3), (5, 4), (5, 5),
-      (3, 2), (4, 2), (3, 5), (4, 5)]
-  
-R0 =[(3, 3), (3, 4),  
-     (4, 3), (4, 4)]
-
-RINGS = [R0, R1, R2, R3]
-
-def Φ(state):
-    X, O = 1, 0
-    board = state.board
-    opp_b = State(-1*board).board
-    X_stacks = [(x, y) for x, y in ALL if board[x][y] > 0]
-    O_stacks = [(x, y) for x, y in ALL if board[x][y] < 0]
-    X_stacks_by_size = [[(x, y) for x, y in X_stacks if board[x][y] ==  stack_size] for stack_size in range(1, 13)]
-    O_stacks_by_size = [[(x, y) for x, y in O_stacks if board[x][y] == -stack_size] for stack_size in range(1, 13)]
-
-    def largest_connected_cluster(player):
-        '''
-        largest connected cluster in terms of number of stacks
-        '''
-        player_stacks = X_stacks.copy() if player == X else O_stacks.copy()
-        colour = 1 if player == X else -1
-        adj = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-        
-        largest_connected_cluster = 0
-        
-        while len(player_stacks) > 0:
-            cur_piece = player_stacks[0]
-            x,y = cur_piece
-            are_adj = set()
-            checked_adj = set()
-            are_adj.add((x,y))
-            while len(are_adj) > len(checked_adj):
-                x,y = are_adj.difference(checked_adj).pop()
-                for d in adj:
-                    dx, dy = x+d[0], y+d[1]
-                    if 0 <= dx < 8 and 0 <= dy < 8:
-                        if board[dx][dy]*colour > 0:
-                            are_adj.add((dx, dy))
-                checked_adj.add((x,y))
-                player_stacks.remove((x,y))
-            if len(are_adj) > largest_connected_cluster:
-                largest_connected_cluster = len(are_adj)
-
-        return largest_connected_cluster
-
-    def largest_almost_connected_cluster(player):
-        ''' 
-        number of pieces in an extended cluster
-        (vulnerable to one opposing stack in the right spot)
-        this is probably hard to program
-        '''
-        pass
-
-    def piece_centrality(player, ring):
-        ''' Overall centrality in terms of where pieces are '''
-        # player_stacks is a list of (x, y) of each stack
-        player_stacks = X_stacks if player == X else O_stacks
-        colour = 1 if player == X else -1
-        
-        count = 0
-        for pos in player_stacks:
-            if pos in ring:
-                count += board[pos[0]][pos[1]]
-        return count*colour
-
-    def stack_centrality(player, ring, stack_size=ALL_STACKS):
-        ''' Overall centrality in terms of where stacks are (ignores that bigger stacks have more pieces) '''
-        # player_stacks is a list of (x, y) of each stack
-        if stack_size == ALL_STACKS:
-            player_stacks = X_stacks if player == X else O_stacks
-        else:
-            player_stacks = X_stacks_by_size[stack_size-1] if player == X else O_stacks_by_size[stack_size-1]
-        
-        count = 0
-        for pos in player_stacks:
-            if pos in ring:
-                count += 1
-        return count
-
-    def spacing(player):
-        pass
-
-    def mobility(player, stacksize=ALL_STACKS):
-        height = abs(board[x][y])
-
-        # all possible move positions on the board, including moves on to opposing pieces
-        all_coords = []
-        for dx in range(1, height+1):
-            if 0 <= x+dx < 8:
-                all_coords.append((x+dx, y))
-            if 0 <= x-dx < 8:
-                all_coords.append((x-dx, y))
-
-        for dy in range(1, height+1):
-            if 0 <= y+dy < 8:
-                all_coords.append((x, y+dy))
-            if 0 <= y-dy < 8:
-                all_coords.append((x, y-dy))
-
-        # make sure we dont move onto an opposing colour
-        all_coords = [(x1, y1) for x1, y1 in all_coords if (board[x1][y1] == 0) or (board[x1][y1] > 0)]
-
-        # append all the moves with all possible number of pieces moved
-        for n in range(1, height+1):
-            for x_dest, y_dest in all_coords:
-                actions.append([MOVE, n, (x, y), (x_dest, y_dest)])
-
-    def control(player):
-        ''' Blowing up all pieces now, how many squares are touched '''
-        player_stacks = X_stacks if player == X else O_stacks
-        
-
-    def control2(player):
-        ''' Moving and then blowing up, how many squares are touched '''
-        pass
-
-    def best_trade(player):
-        ''' piece advantage of best trade '''
-        pass
-
-    def av_cluster_size(player):
-        pass
-
-    
-
-    def pieces(player):
-        if player == X:
-            return board[board > 0].sum()
-        return -board[board < 0].sum()
-    
-    def stacks(player, stack_size=ALL_STACKS):
-        if stack_size == ALL_STACKS:
-            if player == X:
-                return (board > 0).sum()
-            return (board < 0).sum()
-            
-        if player == X:
-            return (board == stack_size).sum()
-        return (board == -stack_size).sum()
-    
-    def actions(player):
-        if player == X:
-            return len(State(board).actions())
-        return len(State(opp_b).actions())
-    
-    def connectivity(player):
-        player_stacks = X_stacks if player == X else O_stacks
-        colour = 1 if player == X else -1
-        adj = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-        
-        count = 0
-        s = set()
-        for x, y in player_stacks:
-            for d in adj:
-                dx, dy = x+d[0], y+d[1]
-                if 0 <= dx < 8 and 0 <= dy < 8:
-                    if board[dx][dy]*colour > 0:
-                        s.add((dx, dy))
-        return len(s)
-            
-    def threat(player):
-        player_stacks = X_stacks if player == X else O_stacks
-        colour = 1 if player == X else -1
-        adj = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-        
-        count = 0
-        s = set()
-        for x, y in player_stacks:
-            for d in adj:
-                dx, dy = x+d[0], y+d[1]
-                if 0 <= dx < 8 and 0 <= dy < 8:
-                    if board[dx][dy]*colour < 0:
-                        s.add((dx, dy))
-        return len(s)
-    
-    def av_stack_size(player):
-        return pieces(player) / stacks(player)
-            
-    # Distance to opponent
-    # Measure of closeness or spread
-    # Board position
-    # Closeness to centre
-
-    functions = [pieces, stacks, actions, connectivity, threat, av_stack_size]
-    return np.array([f(player) for f in functions for player in [X, O]] +
-                    [f(X) - f(O) for f in functions] + 
-                    [])
 
 def H(features, θ):
     h = np.dot(features, θ)
@@ -217,10 +16,11 @@ def H(features, θ):
         return -0.99*INF
     return h
 
-α = 0.0001
+α = 0.00001
 λ = 0.5
 MAX_CHANGE = 0.1
-def tree_strap_train(θ, depth=TRAIN_DEPTH):
+def tree_strap_train(θo, θm, θe, depth=TRAIN_DEPTH, tdl=False):
+    OPN, MID, END = 0, 1, 2
     state = State()
     random_turns = np.random.choice([0] + [2]*2 + [4]*4 + [8]*8 + 16*[16] + 32*[32])
     while (not state.terminal_test()):
@@ -228,6 +28,12 @@ def tree_strap_train(θ, depth=TRAIN_DEPTH):
         print(state)
         print()
 
+        if state.board[state.board > 0].sum() == 12:
+            θ = θo
+        elif state.board[state.board > 0].sum() > 5: 
+            θ = θm
+        else:
+            θ = θe
 
         state.history[state] += 1
 
@@ -236,7 +42,12 @@ def tree_strap_train(θ, depth=TRAIN_DEPTH):
             state = state.result(state.actions(False)[np.random.choice([i for i in range(num_actions)])])
         else:
             searched_states = []
-            V = minimax(State(state.board), depth, θ, searched_states)
+            if tdl:
+                V = negamax(State(state.board), -INF, INF, depth, θ)
+                features = Φ(state)
+                searched_states = [(State(state.board), V, H(features, θ), features, depth)]
+            else:
+                V = minimax(State(state.board), depth, θ, searched_states)
 
             Δθ = np.zeros(num_features)
             for s, vs, hs, features, d in searched_states:
@@ -255,15 +66,16 @@ def tree_strap_train(θ, depth=TRAIN_DEPTH):
             θ += Δθ
 
             actions = []
+            actions2 = []
             for a in state.actions():
                 child = state.result(a)
-                actions.append((-minimax(State(child.board*-1), depth-1, θ), a))
+                actions.append((-negamax(State(-1*child.board), -INF, INF, depth-1, θ), a))
                 
             state = state.result(max(actions)[1])
-        
-        state.board *= -1       
+
+        state.board *= -1
         state.turn += 1
-    return θ
+    return θo, θm, θe
 
 def minimax(state, depth, θ, searched_states=None):
     if state.terminal_test():
@@ -283,36 +95,123 @@ def minimax(state, depth, θ, searched_states=None):
     return maxEval
 
 
-N_GAMES = 5
+def negamax(state, alpha, beta, depth, θ):
+    if state.terminal_test():
+        return state.utility()
+    if depth == 0:
+        return H(Φ(state), θ)
+
+    v = -INF
+    for a in state.actions():
+        child = state.result(a)
+        # game state must be flipped
+        v = max(v, -negamax(State(-1*child.board), alpha, beta, depth-1, θ))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+    return v
+
+def negamax2(state, depth, θ):
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
+
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta, depth):
+        if state.terminal_test():
+            return state.utility()
+        if depth == 0:
+            return H(Φ(state), θ)
+
+        v = -INF
+        for a in state.actions():
+            child = state.result(a)
+            # game state must be flipped
+            v = max(v, -max_value(State(-1*child.board), best_score, beta, depth-1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    # Body of alpha_beta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    best_score = v = -INF
+    beta = +INF
+    best_action = None
+    for a in state.actions():
+        child = state.result(a)
+        # game state must be flipped
+        v = max(v, -max_value(State(-1*child.board), best_score, beta, depth-1))
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
+
+
+N_GAMES = 2
 def main():
-    θ = np.array([-0.31923688,  0.25083284, -0.30244211, -0.0489124,  -0.3328332,   0.09468402,
-  0.48055289,  0.47096562,  0.07606004,  0.04764147, -0.37635794, -0.09468402,
- -0.16457624,  0.29515123,  0.4352868,  -0.73503295, -0.01776477, -0.24063195])
-    θs = [np.copy(θ)]
+    θo = np.array([-0.31923688,  0.25083284, -0.30244211, -0.0489124,  -0.3328332,   0.09468402,
+    0.48055289,  0.47096562,  0.07606004,  0.04764147, -0.37635794, -0.09468402,
+    -0.16457624,  0.29515123,  0.4352868,  -0.73503295, -0.01776477, -0.24063195])
+    θm = np.array([-0.31923688,  0.25083284, -0.30244211, -0.0489124,  -0.3328332,   0.09468402,
+    0.48055289,  0.47096562,  0.07606004,  0.04764147, -0.37635794, -0.09468402,
+    -0.16457624,  0.29515123,  0.4352868,  -0.73503295, -0.01776477, -0.24063195])
+    θe = np.array([-0.31923688,  0.25083284, -0.30244211, -0.0489124,  -0.3328332,   0.09468402,
+    0.48055289,  0.47096562,  0.07606004,  0.04764147, -0.37635794, -0.09468402,
+    -0.16457624,  0.29515123,  0.4352868,  -0.73503295, -0.01776477, -0.24063195])
+    θos, θms, θes = [np.copy(θo)], [np.copy(θm)], [np.copy(θe)]
     for _ in range(N_GAMES):
         print('Game #', _)
-        θ = tree_strap_train(θ)
-        θs.append(np.copy(θ))
+        θo, θm, θe = tree_strap_train(θo, θm, θe, True)
+        θos.append(np.copy(θo))
+        θms.append(np.copy(θm))
+        θes.append(np.copy(θe))
         
-
-    fig, ax = plt.subplots(figsize=(20, 10))
-    for i in range(len(θ)):
-        x = [j for j in range(len(θs))]
-        y = [θs[j][i] for j in range(len(θs))]
-        ax.plot(x, y)
-    sns.despine()
-    plt.savefig('weights.png')
-
-    FACTOR=3
     cmap = sns.diverging_palette(10, 133, as_cmap=True)
-    lbls = ['#X', '#O', 'XAdv']
+    fig = plt.figure(figsize=(20, 4))
+    FACTOR=1
+    plt.subplot(3, 1, 1); sns.heatmap([θo], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, xticklabels=[])
+    plt.subplot(3, 1, 2); sns.heatmap([θm], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, xticklabels=[])
+    plt.subplot(3, 1, 3); sns.heatmap([θe], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, xticklabels=[])
+    plt.savefig('Unlabelled-Heatmap.png')
+    
 
-    fig, ax = plt.subplots(figsize=(20, 2))
-    sns.heatmap([θ], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, ax=ax, xticklabels=lbls)
-    plt.savefig('weight.png')
+    fig = plt.figure(figsize=(20, 20))
+    plt.subplot(3, 1, 1)
+    for i in range(len(θo)):
+        x = [j for j in range(len(θos))]
+        y = [θos[j][i] for j in range(len(θos))]
+        plt.plot(x, y)
+        plt.title('Opening', fontsize=16)
+        plt.ylabel('Weight', fontsize=14)
+    sns.despine()
 
-    print(θ)
+    plt.subplot(3, 1, 2)
+    for i in range(len(θm)):
+        x = [j for j in range(len(θms))]
+        y = [θms[j][i] for j in range(len(θms))]
+        plt.plot(x, y)
+        plt.title('Mid-Game', fontsize=16)
+        plt.ylabel('Weight', fontsize=14)
+    sns.despine()
 
+    plt.subplot(3, 1, 3)
+    for i in range(len(θe)):
+        x = [j for j in range(len(θes))]
+        y = [θes[j][i] for j in range(len(θes))]
+        plt.plot(x, y)
+        plt.title('Ending', fontsize=16)
+        plt.ylabel('Weight', fontsize=14)
+        plt.xlabel('Game', fontsize=14)
+    sns.despine()
+    plt.savefig('Training.png')
+
+    lbls = []
+    fig = plt.figure(figsize=(4, 40))
+    plt.subplot(1, 3, 1); sns.heatmap([[e] for e in θo], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, yticklabels=lbls, xticklabels=[])
+    plt.subplot(1, 3, 2); sns.heatmap([[e] for e in θm], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, yticklabels=[], xticklabels=[])
+    plt.subplot(1, 3, 3); sns.heatmap([[e] for e in θe], cmap=cmap, vmin=-FACTOR, vmax=FACTOR, yticklabels=[], xticklabels=[])
+    plt.savefig('Labelled-Heatmap.png')
+    
 num_features = len(Φ(State()))
 if __name__ == '__main__':
     main()
